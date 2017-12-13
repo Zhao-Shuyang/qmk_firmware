76267a57788c8192fead4926ffb75986e830ae99
suujon <shuyang.zhao@tut.fi>
2017-12-13 16:05:12 +0200

    new

--- keyboards/ergodox_ez/keymaps/nordic-zsy/keymap.c
+++ keyboards/ergodox_ez/keymaps/nordic-zsy/keymap.c
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |  1   |  2 @ |  3 £ |  4 $ |  5 € | 6    |           | ¨ ~  |  7 { |  8 [ |  9 ] |  0 } |  + \ |   Space|
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    | Q    | W    | E    | R    | T    | TT   |           |  TT  | Y    | U    | I    | O    | P    | Enter  |
- * |--------+------+------+------+------+------| (L1) |           | (L2) |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| (L2) |           | (L1) |------+------+------+------+------+--------|
  * | LCrtl  | A    | S    | D    | F    | G    |------|           |------| H    | J    | K    | L    | *    | Enter  |
  * |        |      |      |      |      |      |      |           |      |      |      |      |      | ,    |        |
  * |--------+------+------+------+------+------| Del  |           | Hyper|------+------+------+------+------+--------|
- * | LShft  | Z    | X    | C    | V    | B    |      |           |      | N    | M    | ;    | :    | Up   |   _    |
- * |        |      |      |      |      |      |      |           |      |      |      | ,    | .    |      |   -    |
+ * | LShft  | Z    | X    | C    | V    | B    |      |           |      | N    | M    | ;    | :    | Up   | Rshift |
+ * |        |      |      |      |      |      |      |           |      |      |      | ,    | .    |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtl | <    | LAlt | Space| Space|                                       | AltGr|TT(L1)| Left | Down | Right|
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   | LCtl | >    | _    | LAlt | Space|                                       | AltGr|TT(L1)| Left | Down | Right|
+ *   |      | < |  | -    |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | LGUI |  Ins |       | Home |  End  |
@@ -54,19 +54,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
     // left hand
     KC_ESC,          KC_1,     KC_2,     KC_3,    KC_4,    KC_5,   KC_6,
-    KC_TAB,          KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,   TT(FN),
+    KC_TAB,          KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,   TT(MOUSE),
     KC_LCTRL,        KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
     KC_LSFT,         KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,   KC_DELT,
-    KC_LCTRL,        NO_LESS,  KC_LALT,  KC_SPC, KC_SPC,
+    KC_LCTRL,        NO_LESS,  NO_MINS,  KC_LALT, KC_SPC,
     KC_LGUI,  KC_INSERT,
     KC_F2,
     KC_SPC, KC_MEH, KC_F1,
 
     // right hand
     NO_QUOT, KC_7,   KC_8,    KC_9,    KC_0,    NO_PLUS,  KC_BSPC,
-    TT(MOUSE),   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_ENT,
+    TT(FN),   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_ENT,
              KC_H,   KC_J,    KC_K,    KC_L,    NO_APOS,  KC_ENT,
-    KC_HYPR, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_UP,    NO_MINS,
+    KC_HYPR, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_UP,    KC_RSFT,
                      NO_ALGR, TT(FN),  KC_LEFT, KC_DOWN,  KC_RGHT,
     KC_HOME,   KC_END, 
     KC_PGUP,
@@ -118,18 +118,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Funtion and movement
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |        |
+ * |        |   1  |   2  |   3  |   4  |   5  |   6  |           |      |   7  |   8  |    9 |    0 |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | Up   | PgUp |      |      |           |      |      | LClk | MsUp | RClk |      |        |
+ * |        |      |      | Up   | PgUp |      |      |           |      |      | WhUp | MsUp |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | Home | Left | Down | Rght | End  |------|           |------| LClk | MsLf | MsDn | MsRt |      |        |
+ * |        | Home | Left | Down | Rght | End  |------|           |------| LClk | MsLf | MsDn | MsRt |RClk  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      | PgDn |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      | PgDn |      |      |           |      |      | WhDn |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      | INS  |       |      |      |
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
  *                                 |      |      |------|       |------|      |      |
@@ -138,19 +138,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MOUSE] = LAYOUT_ergodox(
-       KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
+       KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_PGUP, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_INSERT,
+                                           KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       KC_F7,  KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_BTN1, KC_MS_UP, KC_BTN2, KC_TRNS, KC_TRNS,
-                 KC_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_MS_WH_UP, KC_MS_UP, KC_BTN2, KC_TRNS, KC_TRNS,
+                 KC_BTN1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_BTN2, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
